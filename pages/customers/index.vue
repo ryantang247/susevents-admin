bbf3ba3df1d99278c1f9d7065928649dd12ad71c
ryantang <ryantang_register@hotmail.com>
2024-05-16 23:20:08 +0800

    Change Chat configuration for testing in production

--- pages/customers/index.vue
+++ pages/customers/index.vue
@@ -15,8 +15,8 @@
 import MenuList from "~/components/dashboard/MenuList.vue";
 import Header from "~/components/dashboard/Header.vue";
 import VenueCards from "~/components/dashboard/venueCards.vue";
-// import { PrettyChatWindow } from "react-chat-engine-pretty";
-// import { ChatFeed,ChatEngineWrapper,ChatSocket } from 'react-chat-engine';
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { ChatFeed,ChatEngineWrapper,ChatSocket } from 'react-chat-engine';
 import { applyReactInVue } from "veaury";
 import userconfig from '@/userconfig.json';
 
