3473adbe61c4b7bdc8722bc38c071de332b163df
Sreyny THA <sreynymeni@gmail.com>
2024-05-25 22:31:51 +0800

    playwright test

---
+++ playwright/tests/test-2.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://susevents-admin.vercel.app/');
+  await page.locator('#input-0').click();
+  await page.locator('#input-0').fill('124');
+  await page.locator('#input-2').click();
+  await page.locator('#input-2').fill('124');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.getByRole('link', { name: 'Events' }).click();
+  await page.getByRole('link', { name: 'Avatar Add Event' }).click();
+  await page.getByLabel('Event Name').click();
+  await page.getByLabel('Event Name').fill('tennis');
+  await page.getByLabel('Event Name').press('ControlOrMeta+a');
+  await page.getByLabel('Event Name').press('ControlOrMeta+c');
+  await page.getByLabel('Organizer').click();
+  await page.getByLabel('Organizer').fill('tennis');
+  await page.getByLabel('Description').click();
+  await page.getByLabel('Description').fill('tennis');
+  await page.getByLabel('Venue').click();
+  await page.locator('div:nth-child(4) > .v-card > .v-responsive > .v-img__img').click();
+  await page.locator('#input-18').click();
+  await page.getByRole('button', { name: '29' }).click();
+  await page.getByLabel('Contact Number').fill('1235544343');
+  await page.locator('div').filter({ hasText: /^Select category$/ }).nth(3).click();
+  await page.getByText('Competition').click();
+  await page.getByLabel('Select Event Thumbnail', { exact: true }).click();
+  await page.getByLabel('Select Event Thumbnail', { exact: true }).setInputFiles('Screenshot 2023-11-13 125637.png');
+  await page.getByPlaceholder('Enter capacity').click();
+  await page.getByPlaceholder('Enter capacity').fill('100');
+  await page.getByPlaceholder('Enter category').click();
+  await page.getByPlaceholder('Enter category').fill('hello');
+  await page.getByPlaceholder('Enter price').click();
+  await page.getByPlaceholder('Enter price').fill('100');
+  await page.getByRole('button', { name: 'submit' }).click();
+});
