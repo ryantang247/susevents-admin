97f3422d11ffe6e3c4a7fe40b53be352b8787fe2
ryantang <ryantang_register@hotmail.com>
2024-05-28 11:55:44 +0800

    Change redirect route after creating users adn change playwright config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'chrome',
       use: { ...devices['Desktop Chrome'] },
     },
 
