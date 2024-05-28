a9e33435f95372769e97fa76f813dd2b81e3ebf0
Sreyny THA <sreynymeni@gmail.com>
2024-05-28 21:46:38 +0800

    Revert "Playwright test"
    This reverts commit 4b6051127d7b2f14600299ed274b3d3cdaa69cec.

--- playwright/tests/test-3.spec.ts
+++ playwright/tests/test-3.spec.ts
@@ -7,25 +7,38 @@ test('test', async ({ page }) => {
   await page.locator('#input-2').click();
   await page.locator('#input-2').fill('124');
   await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.goto('https://susevents-admin.vercel.app/');
+  await page.goto('https://susevents-admin.vercel.app/user');
+  await page.getByRole('link', { name: 'Events' }).click({
+    button: 'right'
+  });
   await page.getByRole('link', { name: 'Events' }).click();
   await page.getByRole('link', { name: 'Avatar Add Event' }).click();
   await page.getByLabel('Event Name').click();
-  await page.getByLabel('Event Name').fill('tennis');
+  await page.getByLabel('Event Name').fill('dinner');
   await page.getByLabel('Event Name').press('ControlOrMeta+a');
   await page.getByLabel('Event Name').press('ControlOrMeta+c');
   await page.getByLabel('Organizer').click();
-  await page.getByLabel('Organizer').fill('tennis');
+  await page.getByLabel('Organizer').fill('dinner');
   await page.getByLabel('Description').click();
-  await page.getByLabel('Description').fill('tennis');
+  await page.getByLabel('Description').fill('dinner');
   await page.getByLabel('Venue').click();
-  await page.locator('div:nth-child(4) > .v-card > .v-responsive > .v-img__img').click();
+  await page.getByRole('dialog').getByRole('img').first().click();
+  await page.getByText('Start date').click({
+    button: 'right'
+  });
+  await page.getByText('Start date').click();
+  await page.locator('#input-18').click({
+    button: 'right'
+  });
   await page.locator('#input-18').click();
   await page.getByRole('button', { name: '29' }).click();
-  await page.getByLabel('Contact Number').fill('1235544343');
+  await page.getByRole('button', { name: '31' }).click();
+  await page.getByLabel('Contact Number').fill('123434');
   await page.locator('div').filter({ hasText: /^Select category$/ }).nth(3).click();
-  await page.getByText('Competition').click();
+  await page.getByRole('option', { name: 'Fair' }).click();
   await page.getByLabel('Select Event Thumbnail', { exact: true }).click();
-  await page.getByLabel('Select Event Thumbnail', { exact: true }).setInputFiles('Screenshot 2023-11-13 125637.png');
+  await page.getByLabel('Select Event Thumbnail', { exact: true }).setInputFiles('event.png');
   await page.getByPlaceholder('Enter capacity').click();
   await page.getByPlaceholder('Enter capacity').fill('100');
   await page.getByPlaceholder('Enter category').click();
